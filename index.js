afb29373faa51842d27bdaf4b5c8f17f7763fea3
CactusKing101 <chalkking17@gmail.com>
2021-03-05 00:13:05 -0500

    multiple server

--- index.js
+++ index.js
@@ -9,20 +9,27 @@ var total_minutes = data.total_minutes;
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
   function check() {
-    const channel = client.guilds.cache.get(config.guild_id).channels.cache.get(config.channel_id);
-    if (channel.members.has('576154421579481090') && channel.members.has('473110112844644372')) {
-      let tempTotalMinutes = total_minutes + config.interval;
-      let tempHours = Math.floor(tempTotalMinutes / 60);
-      let tempMinutes = tempTotalMinutes % 60;
-      var tempData = {
-        hours: tempHours,
-        minutes: tempMinutes,
-        total_minutes: tempTotalMinutes
-      };
-      let json = JSON.stringify(tempData);
-      total_minutes = tempTotalMinutes;
-      fs.writeFileSync('general/data.json', json);
-      client.user.setActivity(`${tempHours} hours and ${tempMinutes} minutes`);
+    for(let i = 0; i < config.guild_id.length; ++i) {
+      for(let j = 0; j < config.channel_id.length; ++j) {
+        if (!client.guilds.cache.has(config.guild_id[i])) continue;
+        if (!client.guilds.cache.get(config.guild_id[i]).channels.cache.has(config.channel_id[j])) continue;
+        const channel = client.guilds.cache.get(config.guild_id[i]).channels.cache.get(config.channel_id[j]);
+        if (channel.members.has('576154421579481090') && channel.members.has('473110112844644372')) {
+          let tempTotalMinutes = total_minutes + config.interval;
+          let tempHours = Math.floor(tempTotalMinutes / 60);
+          let tempMinutes = tempTotalMinutes % 60;
+          var tempData = {
+            hours: tempHours,
+            minutes: tempMinutes,
+            total_minutes: tempTotalMinutes
+          };
+          let json = JSON.stringify(tempData);
+          total_minutes = tempTotalMinutes;
+          fs.writeFileSync('general/data.json', json);
+          client.user.setActivity(`${tempHours} hours and ${tempMinutes} minutes`);
+          return;
+        }
+      }
     }
   }
   setInterval(check, 60000 * config.interval);

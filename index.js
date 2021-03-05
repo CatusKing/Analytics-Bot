20989f972442f6d251ea8ee393f0913373415911
CactusKing101 <chalkking17@gmail.com>
2021-03-05 08:07:00 -0500

    messages

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const fs = require('fs');
 const token = require('./general/token.json');
 const data = require('./general/data.json');
 const config = require('./general/config.json');
+const dataMsg = require('./general/messages.json');
 const client = new Discord.Client();
 var total_minutes = data.total_minutes;
+var hours = data.hours;
+var minutes = data.minutes;
+var thomasMsg = dataMsg.thomas;
+var joannaMsg = dataMsg.joanna;
+var cycle = 1;
 
 //Started Tracking on 3/4/2021
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+  function status() {
+    if (cycle == 1) {
+
+    } else if (cycle == 2) {
+
+    } else if (cycle == 3) {
+
+    }
+  }
+  setInterval(status, 60000 * config.status_interval)
   function check() {
     for(let i = 0; i < config.guild_id.length; ++i) {
       for(let j = 0; j < config.channel_id.length; ++j) {
@@ -17,7 +33,7 @@ client.once('ready', () => {
         if (!client.guilds.cache.get(config.guild_id[i]).channels.cache.has(config.channel_id[j])) continue;
         const channel = client.guilds.cache.get(config.guild_id[i]).channels.cache.get(config.channel_id[j]);
         if (channel.members.has('576154421579481090') && channel.members.has('473110112844644372')) {
-          let tempTotalMinutes = total_minutes + config.interval;
+          let tempTotalMinutes = total_minutes + config.check_interval;
           let tempHours = Math.floor(tempTotalMinutes / 60);
           let tempMinutes = tempTotalMinutes % 60;
           var tempData = {
@@ -27,15 +43,43 @@ client.once('ready', () => {
           };
           let json = JSON.stringify(tempData);
           total_minutes = tempTotalMinutes;
+          hours = tempHours;
+          minutes = tempMinutes;
           fs.writeFileSync('general/data.json', json);
           console.log(`${tempHours} hours and ${tempMinutes} minutes`);
-          client.user.setActivity(`${tempHours} hours and ${tempMinutes} minutes`);
           return;
         }
       }
     }
   }
-  setInterval(check, 60000 * config.interval);
+  setInterval(check, 60000 * config.check_interval);
+});
+
+client.on('message', msg => {
+  //Joanna
+  if (msg.author.id == '576154421579481090') {
+    let tempJoannaMsg = joannaMsg + 1;
+    var tempData = {
+      thomas: thomasMsg,
+      joanna: tempJoannaMsg
+    };
+    let json = JSON.stringify(tempData);
+    joannaMsg = tempJoannaMsg;
+    fs.writeFileSync('general/messages.json', json);
+    console.log(`Joanna: ${tempJoannaMsg} messages`);
+    return;
+  } else if (msg.author.id == '473110112844644372') {
+    let tempThomasMsg = thomasMsg + 1;
+    var tempData = {
+      thomas: tempThomasMsg,
+      joanna: joannaMsg
+    };
+    let json = JSON.stringify(tempData);
+    thomasMsg = tempThomasMsg;
+    fs.writeFileSync('general/messages.json', json);
+    console.log(`Thomas: ${tempThomasMsg} messages`);
+    return;
+  }
 });
 
 client.login(token.discord);
